efd4b34d2d2b88b627c78672589e9589e136fcd8
biancaandronie <andronie27@gmail.com>
2017-04-05 18:32:00 +0300

    Create time_server.py

---
+++ TD3/time_server.py
@@ -0,0 +1,28 @@
+"""
+@author Andronie Bianca
+@version 0.1
+@date 05.04.2017
+"""
+import socket
+import time
+
+
+# get local machine name
+host = socket.gethostname()
+
+port = 6666
+# create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# bind to the port
+serversocket.bind((host, port))
+
+serversocket.setblocking(0)
+
+print "Le serveur a commence."
+while True:
+    try:
+        data, addr = serversocket.recvfrom(1024)   
+        print time.ctime(time.time()) + str(addr) + ": " + str(data)
+    except:
+		    pass
+serversocket.close()
